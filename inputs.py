d8c769e8521295976460ddafd4c2190d051bf57e
JovanST <jovanST@users.noreply.github.com>
2019-12-09 13:51:49 +0100

    Inputs

---
+++ inputs.py
@@ -0,0 +1,18 @@
+# Practicing inputs
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Your name is: " + name.capitalize() +
+      "\nYour age is: " + age)
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+result = int(num1) + float(num2)
+print(result)
+
+color = input("Enter a color: ")
+plural = input("Enter a plural noun: ")
+celeb = input("Enter a celebrity: ")
+print("Roses are " + color + ",")
+print(plural + " are blue,")
+print("I love " + celeb.capitalize() + ".")
