bbf737c55f581b3ea3bf0fde1b2f84b40a55b469
Jovan_ST <jovan.stankovic70@gmail.com>
2019-12-09 16:45:29 +0100

    Touples

---
+++ touples.py
@@ -0,0 +1,6 @@
+# Working with tuples
+# Touple can't be changed, It's immutable
+# Used for storing data which is not meant to be changed
+
+coordinates = [(3, 4), (56,13), (41,90)]
+print(coordinates[1])
