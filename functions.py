f7d018bc236bf8dbd851376c4f53ef670412365f
Jovan_ST <jovan.stankovic70@gmail.com>
2019-12-09 17:04:49 +0100

    Functions

---
+++ functions.py
@@ -0,0 +1,8 @@
+# Functions
+
+def say_hi(name, age):  # Creating a function with parameters
+    print("Hello " + name + ", you are " + str(age))
+
+
+say_hi("Carl", 32)  # Calling a function; passing parameter to func
+say_hi("Caesar", 12)
